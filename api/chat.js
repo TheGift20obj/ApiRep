630bc9e9cecffd331466254f21ae1d093a900d97
TheGift20obj <djadunov@gmail.com>
2025-07-29 21:53:19 +0200

    Headers

--- api/chat.js
+++ api/chat.js
@@ -3,6 +3,10 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   const { message } = req.body;
 
   try {
